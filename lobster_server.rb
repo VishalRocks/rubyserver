78925a1f49cdeac5c1f9797326a22f1ab2949f0e
Vishal Sharma <sharmavishal94@gmail.com>
2017-01-28 00:50:23 +0530

    Makes it able to serve any rails project

--- lobster_server.rb
+++ lobster_server.rb
@@ -1,8 +1,7 @@
 require 'socket'
-require 'rack'
-require 'rack/lobster'
+require_relative '../RubymineProjects/betscore/config/environment'
 
-app = Rack::Lobster.new
+app = Rack::Lint.new(Rails.application)
 server = TCPServer.new 5678
 
 while session = server.accept
@@ -12,11 +11,23 @@ while session = server.accept
 	http_method, full_path = request.split(' ')
 	path, params = full_path.split('?')
 	
+	input = StringIO.new
+        input.set_encoding 'ASCII-8BIT'
 	puts path,params
 	status, headers, body = app.call({
 		'REQUEST_METHOD' => http_method,
 		'PATH_INFO' => path,
-		'QUERY_STRING' => params
+		'QUERY_STRING' => params || '',
+    		'SERVER_NAME' => 'localhost',
+	        'SERVER_PORT' => '5678',
+		'REMOTE_ADDR' => '127.0.0.1',
+	        'rack.version' => [1,3],
+	        'rack.input' => input,
+       	        'rack.errors' => $stderr,
+	        'rack.multithread' => false,
+	        'rack.multiprocess' => false,
+	        'rack.run_once' => false,
+	        'rack.url_scheme' => 'http'
 	})
 	
 	session.print "HTTP/1.1 #{status}\r\n"
